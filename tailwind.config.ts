7e729944e6bdfed1d6f596bfd35db5b0b05c0d39
Manuel Blanco <274275174+mblancolabs@users.noreply.github.com>
2026-05-02 00:45:23 +0200

    Fixed compatibility issues with the DarkModeStrategy type

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import tailwindcssAnimate from "tailwindcss-animate";
 import typography from "@tailwindcss/typography";
 
 export default {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
